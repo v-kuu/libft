649cbebb9ce8435515abc7e484be6da00b6a1fda
Ville Kuusela <vkuusela@c2r6p9.hive.fi>
2024-10-31 10:16:10 +0200

    hello

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vkuusela <vkuusela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/30 15:57:17 by vkuusela          #+#    #+#             */
-/*   Updated: 2024/10/30 16:02:41 by vkuusela         ###   ########.fr       */
+/*   Created: 2024/10/31 10:12:39 by vkuusela          #+#    #+#             */
+/*   Updated: 2024/10/31 10:14:06 by vkuusela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <aio.h>
 #include <stdlib.h>
 
-char	*ft_substr(char const *string, unsigned int start, size_t length)
+char	*ft_strtrim(char const *string, char const *set)
 {
-	char	*substring;
-
-	substring = (char *)malloc(length * sizeof(char));
-	while (length > 0)
-	{
-		*substring = string[start];
-		substring++;
-		start++;
-		length--;
-	}
-	*substring = 0;
-	return (substring);
+	
 }
