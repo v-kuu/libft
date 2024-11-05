74502d3501ead7369b8e06043f0fa9ffff5f3fe2
Ville Kuusela <vkuusela@student.hive.fi>
2024-11-05 14:32:00 +0200

    im int maxxing

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,37 +12,39 @@
 
 #include "libft.h"
 
-static int	ft_digit_count(int integer);
+static long	ft_digit_count(long integer);
 
 char	*ft_itoa(int integer)
 {
 	char	*string;
 	int		sign;
 	int		size;
+	long	number;
 
-	sign = (integer < 0);
-	size = ft_digit_count(integer);
+	number = integer;
+	sign = (number < 0);
+	size = ft_digit_count(number);
 	if (sign)
-		integer *= -1;
+		number *= -1;
 	string = (char *)ft_calloc(size + sign + 1, sizeof(char));
 	if (string == 0)
 		return (0);
 	if (integer == 0)
 		string[0] = '0';
-	while (integer > 0)
+	while (number > 0)
 	{
-		string[size - 1] = integer % 10 + 48;
+		string[size - 1] = number % 10 + 48;
 		size--;
-		integer /= 10;
+		number /= 10;
 	}
 	if (sign)
 		string[0] = '-';
 	return (string);
 }
 
-static int	ft_digit_count(int integer)
+static long	ft_digit_count(long integer)
 {
-	int	count;
+	long	count;
 
 	count = 1;
 	if (integer < 0)
