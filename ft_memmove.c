3aa7c41aaa5122733e633d135bd980a95d13b41b
Ville Kuusela <vkuusela@student.hive.fi>
2024-11-07 10:37:25 +0200

    header fix

--- ft_memmove.c
+++ ft_memmove.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <aio.h>
+#include "libft.h"
 
 void	*ft_memmove(void *destination, const void *source, size_t length)
 {
